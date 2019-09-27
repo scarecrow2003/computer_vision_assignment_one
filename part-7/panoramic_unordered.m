72e36c0995cb67de217081c275195af004885979
zhihua.su <zhihua@sg.ibm.com>
2019-09-27 23:22:56 +0800

    commit

--- part-7/panoramic_unordered.m
+++ part-7/panoramic_unordered.m
@@ -88,7 +88,8 @@ function stitched = stitch(im01, im02)
     im02gray = single(rgb2gray(im02));
     [f1, d1] = vl_sift(im01gray);
     [f2, d2] = vl_sift(im02gray);
-    [matches, ~] = vl_ubcmatch(d1, d2);
+%     [matches, ~] = vl_ubcmatch(d1, d2);
+    matches = find_matches(d1, d2, 0.9295);
     f1_match = f1(:, matches(1, :));
     f2_match = f2(:, matches(2, :));
     homography = ransac(f1_match, f2_match);
@@ -167,4 +168,33 @@ function stitched = stitch_using_homography(im01, im02, homography)
     im01_transformed = imtransform(im01, maketform('affine',eye(3)), 'XData', xbound, 'YData', ybound, 'XYScale',1);
     im02_transformed = imtransform(im02, tform, 'XData', xbound, 'YData', ybound, 'XYScale',1);
     stitched = max(im01_transformed, im02_transformed);
+end
+
+function matches = find_matches(d1, d2, reject_threshold)
+    d1_size = size(d1, 2);
+    d2_size = size(d2, 2);
+    matches = zeros(2, d1_size);
+    counter = 0;
+    for i=1:d1_size
+        d1_d = d1(:, i);
+        closest_index = 0;
+        closest_value = realmax;
+        second_closest_value = realmax;
+        for j=1:d2_size
+            d2_d = d2(:, j);
+            dist_square = sum((d1_d - d2_d) .^ 2);
+            if dist_square <= closest_value
+                second_closest_value = closest_value;
+                closest_value = dist_square;
+                closest_index = j;
+            elseif dist_square < second_closest_value
+                second_closest_value = dist_square;
+            end
+        end
+        if sqrt(closest_value) / sqrt(second_closest_value) < reject_threshold
+            counter = counter + 1;
+            matches(:, counter) = [i; closest_index];
+        end
+    end
+    matches = matches(:, 1:counter);
 end
